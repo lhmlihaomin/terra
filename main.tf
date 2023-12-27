7e23daa04ed079aaa5353ba6f95417ac527ac66a
Li Haomin <lhmlihaomin@live.com>
2023-12-27 06:45:11 +0000

    fixed security group resource name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_security_group" "sample_sg" {
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress_all" {
-  security_group_id = aws_security_group.sg_influxdb.id
+  security_group_id = aws_security_group.sample_sg.id
   ip_protocol       = -1
   from_port         = -1
   to_port           = -1
